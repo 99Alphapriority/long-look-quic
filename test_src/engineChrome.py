bcee80ceaa1d3d380aeade54611804f66e5878eb
naveenrajm7 <naveenrajm97@gmail.com>
2023-03-12 18:11:04 -0600

    add timeout decorator for chrome open and close

--- test_src/engineChrome.py
+++ test_src/engineChrome.py
@@ -12,11 +12,31 @@ except ImportError:
 # import stats
 # import sideTrafficGenerator
 # from pythonLib import *
-# from functools import wraps
-# import errno
-# import os
-# import signal
+from functools import wraps
+import errno
+import os
+import signal
 
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
 
 class Driver(object):
     def __init__(self, chromeDriverPath, browserPath, options, pageLoadTimeOut=None):
@@ -26,7 +46,7 @@ class Driver(object):
         self.pageLoadTimeOut  = pageLoadTimeOut
         self.driver           = None    
 
-    # @timeout(20)
+    @timeout(20)
     def open(self):
         webdriver.ChromeOptions.binary_location = self.browserPath
         chromeService = webdriver.chrome.service.Service(self.chromeDriverPath)
@@ -36,7 +56,7 @@ class Driver(object):
         if self.pageLoadTimeOut:
             self.driver.set_page_load_timeout(self.pageLoadTimeOut)
 
-    # @timeout(20)
+    @timeout(20)
     def close(self):
         self.driver.close()
         self.driver.quit()
@@ -52,6 +72,8 @@ class Driver(object):
 
 
 def main():
+    #The following line is to make sure the script has sudo privilage to run tcpdump
+    os.system('sudo echo')
 
     # create chrome driver options
     chromeOptions = webdriver.ChromeOptions()
