283e5fa21a43ad4605f020e4cd8a2b2ce48438a6
naveenrajm7 <naveenrajm97@gmail.com>
2023-05-18 19:12:59 -0600

    fix var delays

--- test_src/engineWrapper.py
+++ test_src/engineWrapper.py
@@ -10,7 +10,7 @@ obj_set1  = [ '5k.html', '10k.html', '100k.html', '200k.html', '500k.html', '1mb
 obj_set2  = [ '1mbx1.html','500kx2.html' ,'200kx5.html' ,'100kx10.html', '10kx100.html', '5kx200.html' ]  
 
 # all rates (scenarios)
-ratesX = "10_36_0,50_36_0,100_36_0,10_112_0,50_112_0,100_112_0,10_36_1,50_36_1,100_36_1,10_100J5_0,50_100J5_0,100_100J5_0"
+ratesX = "10_36_0,50_36_0,100_36_0,10_112_0,50_112_0,100_112_0,10_36_1,50_36_1,100_36_1,10_100J10_0,50_100J10_0,100_100J10_0"
 indexX = "5k,10k,100k,200k,500k,1mb,10mb,1mbx1,500kx2,200kx5,100kx10,10kx100,5kx200"
 
 # Configs for running experiments , 
@@ -24,7 +24,7 @@ def initialize():
     configs.set('pythonBinary', '/proj/FEC-HTTP/nenv/bin/python')
     configs.set('mainDir', '')
 
-    configs.set('rates'             ,  "10_36_0,50_36_0,100_36_0,10_112_0,50_112_0,100_112_0,10_36_1,50_36_1,100_36_1,10_100J5_0,50_100J5_0,100_100J5_0")
+    configs.set('rates'             ,  "10_36_0,50_36_0,100_36_0,10_112_0,50_112_0,100_112_0,10_36_1,50_36_1,100_36_1,10_100J10_0,50_100J10_0,100_100J10_0")
     # configs.set('qualities'         , 'hd2160,hd1440,hd1080,hd720,large,medium,small,tiny,auto')
     configs.set('stopTime'          , '60')
     configs.set('indexes'           , "5k,10k,100k,200k,500k,1mb,10mb,1mbx1,500kx2,200kx5,100kx10,10kx100,5kx200")
